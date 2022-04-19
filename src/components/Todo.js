c8713c2e749fcf263bae9634cbff8189ebfb53c6
Kalin Trachev <100349253+KalinTrachev@users.noreply.github.com>
2022-04-19 15:25:56 +0300

    Modify

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,19 +5,22 @@ import Backdrop from './Backdrop';
 function Todo(props) {
     const [modalIsOpen, setModalIsOpen] = useState(false);
 
-        function deleteHandler() {
-            setModalIsOpen(true);
-        }
-
-        return (
-            <div className="card">
-                <h2>{props.text}</h2>
-                <div className="actions">
-                    <button className="btn" onClick={deleteHandler}>Delete</button>
-                </div>
-                {modalIsOpen && <Modal />}
-                {modalIsOpen && <Backdrop />}
-            </div>
-        );
+    function deleteHandler() {
+        setModalIsOpen(true);
     }
+    function closeModalHandler() {
+        setModalIsOpen(false)
+    }
+
+    return (
+        <div className="card">
+            <h2>{props.text}</h2>
+            <div className="actions">
+                <button className="btn" onClick={deleteHandler}>Delete</button>
+            </div>
+            {modalIsOpen && <Modal />}
+            {modalIsOpen && <Backdrop onCancel={closeModalHandler} />}
+        </div>
+    );
+}
 export default Todo;
