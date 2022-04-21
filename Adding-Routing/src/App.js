444a20a937746a41cf913b5e248467c47c5ba7fb
Kalin Trachev <100349253+KalinTrachev@users.noreply.github.com>
2022-04-21 23:00:31 +0300

    Modify

--- Adding-Routing/src/App.js
+++ Adding-Routing/src/App.js
@@ -1,20 +1,22 @@
-import { Route } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom';
+
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetup';
 import FavoritesPage from './pages/Favorites';
 
 function App() {
   return <div>
-    <Route path='/'>
-      <AllMeetupsPage />
-    </Route>
-    <Route path='/new-meetup'>
-      <NewMeetupPage />
-    </Route>
-    <Route path='/favorites'>
-      <FavoritesPage />
-    </Route>
+    <Routes>
+      <Route path='/' element={<AllMeetupsPage />} />
+      <Route path='/new-meetup'>
+        <NewMeetupPage />
+      </Route>
+      <Route path='/favorites'>
+        <FavoritesPage />
+      </Route>
+    </Routes>
+
   </div>;
-}
+};
 
 export default App;
